53b8a362ba07ce25400e91feee60bc6d8d3e3420
vladimirovicp <vladimirovicp70@gmail.com>
2024-04-25 19:32:39 +0400

    refactor: удаление функций от которых отказался

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -35,55 +35,6 @@ const userLogout = (ws,stateUser) => {
   window.location.hash = '#auth';
 }
 
-//?? Аутентификация пользователя третьей стороной
-// Серверное приложение
-const thirdPartyUserAuthentication = () => {
-  /*
-    {
-      id: null,
-      type: "USER_EXTERNAL_LOGIN",
-      payload: {
-        user: {
-          login: string,
-          isLogined: boolean,
-        }
-      }
-    }
-  */
-
-  /*
-    id - идентификатор запроса, сгенерированный сервером
-    login - имя пользователя, вошедшего в систему
-    isLogined - текущий статус аутентификации пользователя
-  */
-
-}
-
-//?? Выход стороннего пользователя из системы
-// Серверное приложение
-const thirdPartyUserLogout = () =>{
-  /*
-    {
-      id: null,
-      type: "USER_EXTERNAL_LOGOUT",
-      payload: {
-        user: {
-          login: string,
-          isLogined: boolean,
-        }
-      }
-    }
-  */
-
-    /*
-    id - идентификатор запроса, сгенерированный сервером
-    login - имя пользователя, вышедшего из приложения
-    isLogined - текущий статус аутентификации пользователя
-    */
-
-
-}
-
 //Получение всех аутентифицированных пользователей
 const gettingAllAuthenticatedUsers = (ws) =>{
   const data = {
@@ -119,50 +70,8 @@ const sendingMessageUser = (ws,textMessage,toUser) =>{
       }
     }
     ws.send(JSON.stringify(data));
-    /*
-      id - идентификатор запроса
-      login - логин пользователя, которому отправляется сообщение
-      text - текст сообщения
-    */
 }
 
-//?? Получение сообщения от пользователя
-// Серверное приложение
-const receivingMessageFromUser = () => {
-  /*
-      {
-      id: null,
-      type: "MSG_SEND",
-      payload: {
-        message: {
-          id: string,
-          from: string,
-          to: string,
-          text: string,
-          datetime: number,
-          status: {
-            isDelivered: boolean,
-            isReaded: boolean,
-            isEdited: boolean,
-          }
-        }
-      }
-    }
-  */
-
-    /*
-      id - идентификатор запроса, сгенерированный сервером
-        message - поле сообщения пользователя, где:
-        id - идентификатор сообщения
-        from - отправитель сообщения
-        to - получатель сообщения
-        text - текст сообщения
-        datetime - дата и время отправки сообщения
-        isDelivered - статус, который указывает, доставлено ли сообщение получателю
-        isReaded - статус, который указывает, было ли сообщение прочитано получателем
-        isEdited - статус, который указывает, было ли сообщение отредактировано отправителем
-    */
-}
 
 //Получение истории сообщений пользователя
 const fetchingMessageHistoryWithUser = (ws,login) => {
@@ -183,32 +92,6 @@ const fetchingMessageHistoryWithUser = (ws,login) => {
     */
 }
 
-//?? Уведомление об изменении статуса доставки сообщения
-// Серверное приложение
-const notificationMessageDeliveryStatusChange = () => {
-  // {
-  //   id: null,
-  //   type: "MSG_DELIVER",
-  //   payload: {
-  //     message: {
-  //       id: string,
-  //       status: {
-  //         isDelivered: boolean,
-  //       }
-  //     }
-  //   }
-  // }
-
-  /**
-   *  id - идентификатор запроса, сгенерированный сервером
-      message - поле сообщения пользователя, где:
-      id - идентификатор сообщения
-      isDelivered - статус, который указывает, доставлено ли сообщение получателю
-   */
-
-
-}
-
 //Изменение статуса прочтения сообщения
 const messageReadStatusChange = (ws,idMessage) => {
     const data = {
@@ -221,10 +104,9 @@ const messageReadStatusChange = (ws,idMessage) => {
       }
     };
     ws.send(JSON.stringify(data));
-
 } 
 
-//?? Удаление сообщения
+//Удаление сообщения
 const messageDeletion = (ws,id) => {
 
   const data = {
@@ -238,48 +120,7 @@ const messageDeletion = (ws,id) => {
       };
 
       ws.send(JSON.stringify(data));
-
-  /**
-   * 
-   * id - идентификатор запроса
-      message - поле сообщения пользователя, где:
-      id - идентификатор сообщения
-   */
-
 }
-
-//?? Уведомление об удалении сообщения
-// Инициатор: Серверное приложение
-const notificationMessageDeletion = () => {
-
-}
-
-//Редактирование текста сообщения
-const messageTextEditing = () =>{
-  /**
-   * {
-        id: string,
-        type: "MSG_EDIT"
-        payload: {
-          message: {
-            id: string,
-            text: string
-          }
-        }
-      }
-   */
-
-      /**
-       * id - идентификатор запроса
-            message - поле сообщения пользователя, где:
-            id - идентификатор сообщения
-            text - текст сообщения
-       */
-}
-
-
-
-
 
 export { 
   userAuthentication,
@@ -288,8 +129,6 @@ export {
   gettingAllUnauthorizedUsers,
   sendingMessageUser,
   fetchingMessageHistoryWithUser,
-  notificationMessageDeliveryStatusChange,
   messageReadStatusChange,
   messageDeletion,
-  messageTextEditing,
  };
